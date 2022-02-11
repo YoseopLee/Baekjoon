18205217d28b77cb6500791f69b42cc0676ccc67
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2022-02-11 15:35:27 +0900

    [ADD]17298

---
+++ b17298/app.js
@@ -0,0 +1,15 @@
+const filePath = process.platform === 'linux' ? 'dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(filePath).toString().split('\n');
+const N = parseInt(input[0]);
+const A = input[1].split(' ').map(v => +v);
+let stack = [];
+for (let i = 0 ; i < A.length; i++) {
+    while (stack.length && A[stack[stack.length - 1]] < A[i]) {
+        A[stack.pop()] = A[i];
+    }
+    stack.push(i);
+}
+while (stack.length) {
+    A[stack.pop()] = -1;    
+}
+console.log(A.join(' '));
