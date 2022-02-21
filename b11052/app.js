d1ab63e11f170b6c169d61dcd8361879ecee4b48
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2022-02-21 18:19:41 +0900

    [ADD]11052

---
+++ b11052/app.js
@@ -0,0 +1,11 @@
+const filePath = process.platform === 'linux' ? 'dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(filePath).toString().split('\n');
+const N = Number(input[0]);
+const P = input[1].split(' ').map(v=>+v);
+let dp = [0, ...P];
+for (let i = 2; i < dp.length; i++) {
+    for (let j = 1; j < i; j++) {
+        dp[i] = Math.max(dp[i], (dp[i-j] + dp[j]));
+    }
+}
+console.log(dp[N]);
