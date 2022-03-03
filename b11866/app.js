de8a34b703388d098c4716043952fde427625f8b
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2022-03-03 15:26:34 +0900

    [ADD]11866

---
+++ b11866/app.js
@@ -0,0 +1,23 @@
+const filePath = process.platform === 'linux' ? 'dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split(' ').map(v=>+v);
+
+const N = input[0];
+const K = input[1];
+
+let A = [];
+let B = [];
+
+for (let i = 1; i <= N; i++) {
+    A.push(i);
+};
+for (let i = 0; i < N; i++) {
+    for (let j = 1; j <= K; j++) {
+        if (j === K) {
+            B.push(A.shift());
+        } else {
+            A.push(A.shift());
+        }
+    }
+}
+
+console.log('<'+ B.join(', ').toString()+'>');
