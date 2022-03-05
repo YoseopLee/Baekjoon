36d6a4692ef0fb71ef589e1b27ab22541f39432e
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2022-03-05 11:35:17 +0900

    [ADD]1654

--- b1654/app2.js
+++ b1654/app2.js
@@ -1,5 +1,6 @@
 const filePath = process.platform === 'linux' ? 'dev/stdin' : './input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
 const [K, N] = input.shift().split(' ').map(v=>+v);
 const inputs = input.map(v=>+v).sort((a,b) => a-b);
 
@@ -10,7 +11,7 @@ let answer = 0;
 while (start <= end) {
     let mid = Math.floor((start + end) / 2);
     let count = inputs.reduce((a,b) => a + Math.floor(b/mid), 0);
-    
+
     if (count >= N) {
         if (mid > answer) {
             answer = mid;
